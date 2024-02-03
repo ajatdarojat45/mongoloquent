5094925e07aa832577aa4c059c2607a5f8279a80
Ajat Darojat <ajatdarojat45@gmail.com>
2024-02-03 22:41:54 +0700

    test: add test for belongsToMany relation

--- __tests__/database/Relation.test.ts
+++ __tests__/database/Relation.test.ts
@@ -74,3 +74,49 @@ describe("Relation - generateHasMany should return this", () => {
     expect(lookups[0]).toHaveProperty("$lookup");
   });
 });
+
+describe("Relation - belongsToMany method", () => {
+  test("should return an object with the collection, foreignKey, localKey and type properties", () => {
+    const result = Relation["belongsToMany"](
+      "products",
+      "users",
+      "userId",
+      "_id"
+    );
+
+    expect(result).toEqual(expect.any(Object));
+    expect(result).toHaveProperty("collection", "products");
+    expect(result).toHaveProperty("foreignKey", "userId");
+    expect(result).toHaveProperty("localKey", "_id");
+    expect(result).toHaveProperty("type", "belongsToMany");
+  });
+});
+
+describe("Relation - generateBelongsToMany should return this", () => {
+  test("should return this", () => {
+    const result = Relation["generateBelongsToMany"]({
+      collection: "products",
+      pivotCollection: "users",
+      foreignKey: "countryId",
+      localKey: "userId",
+      type: "belongsToMany",
+      alias: "products",
+      options: {},
+    });
+
+    expect(result).toEqual(expect.any(Function));
+
+    const lookups = Relation["lookups"];
+    expect(lookups).toEqual(expect.any(Array));
+    expect(lookups).toHaveLength(3);
+
+    expect(lookups[0]).toEqual(expect.any(Object));
+    expect(lookups[0]).toHaveProperty("$lookup");
+
+    expect(lookups[1]).toEqual(expect.any(Object));
+    expect(lookups[1]).toHaveProperty("$lookup");
+
+    expect(lookups[2]).toEqual(expect.any(Object));
+    expect(lookups[2]).toHaveProperty("$project");
+  });
+});
