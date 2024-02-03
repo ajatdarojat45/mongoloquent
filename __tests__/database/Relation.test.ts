a02c037bd26ca4112231deab75c7910f31f5e758
Ajat Darojat <ajatdarojat45@gmail.com>
2024-02-03 22:48:24 +0700

    test: add test for hasManyThrough relation

--- __tests__/database/Relation.test.ts
+++ __tests__/database/Relation.test.ts
@@ -120,3 +120,51 @@ describe("Relation - generateBelongsToMany should return this", () => {
     expect(lookups[2]).toHaveProperty("$project");
   });
 });
+
+describe("Relation - hasManyThrough method", () => {
+  test("should return an object with the collection, foreignKey, localKey and type properties", () => {
+    const result = Relation["hasManyThrough"](
+      "roles",
+      "userRoles",
+      "userId",
+      "_id"
+    );
+
+    console.log(result);
+    expect(result).toEqual(expect.any(Object));
+    expect(result).toHaveProperty("collection", "roles");
+    expect(result).toHaveProperty("throughCollection", "userRoles");
+    expect(result).toHaveProperty("foreignKey", "_id");
+    expect(result).toHaveProperty("localKey", "userId");
+    expect(result).toHaveProperty("type", "hasManyThrough");
+  });
+});
+
+describe("Relation - generateHasManyThrough should return this", () => {
+  test("should return this", () => {
+    const result = Relation["generateHasManyThrough"]({
+      collection: "roles",
+      throughCollection: "userRoles",
+      foreignKey: "roleId",
+      localKey: "userId",
+      type: "hasManyThrough",
+      alias: "roles",
+      options: {},
+    });
+
+    expect(result).toEqual(expect.any(Function));
+
+    const lookups = Relation["lookups"];
+    expect(lookups).toEqual(expect.any(Array));
+    expect(lookups).toHaveLength(3);
+
+    expect(lookups[0]).toEqual(expect.any(Object));
+    expect(lookups[0]).toHaveProperty("$lookup");
+
+    expect(lookups[1]).toEqual(expect.any(Object));
+    expect(lookups[1]).toHaveProperty("$lookup");
+
+    expect(lookups[2]).toEqual(expect.any(Object));
+    expect(lookups[2]).toHaveProperty("$project");
+  });
+});
