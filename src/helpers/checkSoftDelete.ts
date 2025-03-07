a23bc0b755c9c645e51628104d6857dcf4234014
Ajat Darojat <ajatdarojat45@gmail.com>
2025-03-07 09:55:19 +0700

    chore: delete unused files

--- src/helpers/checkSoftDelete.ts
+++
@@ -1,9 +0,0 @@
-function checkSoftDelete(softDelete: boolean, payload: object): object {
-  if (softDelete) {
-    return { ...payload, isDeleted: false };
-  }
-
-  return payload;
-}
-
-export default checkSoftDelete;
