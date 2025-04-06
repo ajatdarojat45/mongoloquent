6366f2cd09358bf6cd480cad3ad28156c042605a
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-06 21:07:23 +0700

    feat(DB): add JSDoc comments for DB class and its methods for improved documentation

--- src/DB.ts
+++ src/DB.ts
@@ -1,12 +1,28 @@
 import QueryBuilder from "./QueryBuilder";
 
+/**
+ * DB class represents a database collection wrapper that extends QueryBuilder
+ * to provide MongoDB-like query operations.
+ * @template T - The type of documents stored in the collection
+ * @extends {QueryBuilder<T>}
+ */
 export default class DB<T> extends QueryBuilder<T> {
+  /** Allow dynamic property access */
   [key: string]: any;
 
+  /**
+   * Creates a new instance of the DB class
+   */
   constructor() {
     super();
   }
 
+  /**
+   * Creates a new DB instance for the specified collection
+   * @template T - The type of documents stored in the collection
+   * @param {string} collection - The name of the collection to operate on
+   * @returns {DB<T>} A new DB instance configured for the specified collection
+   */
   static collection<T>(this: new () => DB<T>, collection: string): DB<T> {
     const q = new this();
     q["$collection"] = collection;
