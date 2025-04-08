663310ab8df073b9f0766f8aec45a60a55386fd2
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-08 21:35:40 +0700

    test: add get method tests for Post and Comment models

---
+++ __tests__/relationships/hasMany/get.test.ts
@@ -0,0 +1,223 @@
+import { ObjectId } from "mongodb";
+
+import DB from "../../../src/DB";
+import Model from "../../../src/Model";
+import {
+  IMongoloquentSchema,
+  IMongoloquentSoftDelete,
+} from "../../../src/interfaces/ISchema";
+
+beforeEach(async () => {
+  await DB.collection("posts").getCollection().deleteMany({});
+  await DB.collection("comments").getCollection().deleteMany({});
+});
+
+describe("get method", () => {
+  describe("without parameters", () => {
+    it("should return all doc", async () => {
+      interface IPost extends IMongoloquentSchema {
+        title: string;
+        content: string;
+        comments?: Comment[];
+      }
+
+      interface IComment extends IMongoloquentSoftDelete {
+        postId: ObjectId;
+        content: string;
+        active: boolean;
+      }
+
+      class Post extends Model<IPost> {
+        static $collection = "posts";
+        static $schema: Post;
+
+        comments() {
+          return this.hasMany(Comment, "postId", "_id");
+        }
+      }
+
+      class Comment extends Model<IComment> {
+        static $collection = "comments";
+        static $useTimestamps = false;
+        static $schema: Comment;
+      }
+
+      const postsIds = await Post.insertMany([
+        { title: "Post 1", content: "Content 1" },
+        { title: "Post 2", content: "Content 2" },
+      ]);
+
+      await Comment.insertMany([
+        { postId: postsIds[0], content: "Comment 1", active: true },
+        { postId: postsIds[0], content: "Comment 2", active: true },
+        { postId: postsIds[1], content: "Comment 3", active: true },
+      ]);
+
+      const post = await Post.find(postsIds[0]);
+
+      const comments = await post.comments().get();
+      expect(comments).toEqual(expect.any(Array));
+      expect(comments).toHaveLength(2);
+      expect(comments[0]).toEqual(expect.any(Object));
+      expect(comments[0]).toHaveProperty("_id");
+      expect(comments[0]).toHaveProperty("postId", post._id);
+      expect(comments[0]).toHaveProperty("content", "Comment 1");
+      expect(comments[0]).toHaveProperty("active", true);
+    });
+  });
+
+  describe("with parameters", () => {
+    it("with single parameter", async () => {
+      interface IPost extends IMongoloquentSchema {
+        title: string;
+        content: string;
+        comments?: Comment[];
+      }
+
+      interface IComment extends IMongoloquentSoftDelete {
+        postId: ObjectId;
+        content: string;
+        active: boolean;
+      }
+
+      class Post extends Model<IPost> {
+        static $collection = "posts";
+        static $schema: Post;
+
+        comments() {
+          return this.hasMany(Comment, "postId", "_id");
+        }
+      }
+
+      class Comment extends Model<IComment> {
+        static $collection = "comments";
+        static $useTimestamps = false;
+        static $schema: Comment;
+      }
+
+      const postsIds = await Post.insertMany([
+        { title: "Post 1", content: "Content 1" },
+        { title: "Post 2", content: "Content 2" },
+      ]);
+
+      await Comment.insertMany([
+        { postId: postsIds[0], content: "Comment 1", active: true },
+        { postId: postsIds[0], content: "Comment 2", active: true },
+        { postId: postsIds[1], content: "Comment 3", active: true },
+      ]);
+
+      const post = await Post.find(postsIds[0]);
+
+      const comments = await post.comments().get("content");
+      expect(comments).toEqual(expect.any(Array));
+      expect(comments).toHaveLength(2);
+      expect(comments[0]).toEqual(expect.any(Object));
+      expect(comments[0]).toHaveProperty("_id");
+      expect(comments[0]).toHaveProperty("content", "Comment 1");
+      expect(comments[0]).not.toHaveProperty("postId", post._id);
+      expect(comments[0]).not.toHaveProperty("active", true);
+    });
+
+    it("with multiple parameters", async () => {
+      interface IPost extends IMongoloquentSchema {
+        title: string;
+        content: string;
+        comments?: Comment[];
+      }
+
+      interface IComment extends IMongoloquentSoftDelete {
+        postId: ObjectId;
+        content: string;
+        active: boolean;
+      }
+
+      class Post extends Model<IPost> {
+        static $collection = "posts";
+        static $schema: Post;
+
+        comments() {
+          return this.hasMany(Comment, "postId", "_id");
+        }
+      }
+
+      class Comment extends Model<IComment> {
+        static $collection = "comments";
+        static $useTimestamps = false;
+        static $schema: Comment;
+      }
+
+      const postsIds = await Post.insertMany([
+        { title: "Post 1", content: "Content 1" },
+        { title: "Post 2", content: "Content 2" },
+      ]);
+
+      await Comment.insertMany([
+        { postId: postsIds[0], content: "Comment 1", active: true },
+        { postId: postsIds[0], content: "Comment 2", active: true },
+        { postId: postsIds[1], content: "Comment 3", active: true },
+      ]);
+
+      const post = await Post.find(postsIds[0]);
+
+      const comments = await post.comments().get("content", "active");
+      expect(comments).toEqual(expect.any(Array));
+      expect(comments).toHaveLength(2);
+      expect(comments[0]).toEqual(expect.any(Object));
+      expect(comments[0]).toHaveProperty("_id");
+      expect(comments[0]).toHaveProperty("content", "Comment 1");
+      expect(comments[0]).toHaveProperty("active", true);
+      expect(comments[0]).not.toHaveProperty("postId", post._id);
+    });
+
+    it("with array parameter", async () => {
+      interface IPost extends IMongoloquentSchema {
+        title: string;
+        content: string;
+        comments?: Comment[];
+      }
+
+      interface IComment extends IMongoloquentSoftDelete {
+        postId: ObjectId;
+        content: string;
+        active: boolean;
+      }
+
+      class Post extends Model<IPost> {
+        static $collection = "posts";
+        static $schema: Post;
+
+        comments() {
+          return this.hasMany(Comment, "postId", "_id");
+        }
+      }
+
+      class Comment extends Model<IComment> {
+        static $collection = "comments";
+        static $useTimestamps = false;
+        static $schema: Comment;
+      }
+
+      const postsIds = await Post.insertMany([
+        { title: "Post 1", content: "Content 1" },
+        { title: "Post 2", content: "Content 2" },
+      ]);
+
+      await Comment.insertMany([
+        { postId: postsIds[0], content: "Comment 1", active: true },
+        { postId: postsIds[0], content: "Comment 2", active: true },
+        { postId: postsIds[1], content: "Comment 3", active: true },
+      ]);
+
+      const post = await Post.find(postsIds[0]);
+
+      const comments = await post.comments().get(["content", "active"]);
+      expect(comments).toEqual(expect.any(Array));
+      expect(comments).toHaveLength(2);
+      expect(comments[0]).toEqual(expect.any(Object));
+      expect(comments[0]).toHaveProperty("_id");
+      expect(comments[0]).toHaveProperty("content", "Comment 1");
+      expect(comments[0]).toHaveProperty("active", true);
+      expect(comments[0]).not.toHaveProperty("postId", post._id);
+    });
+  });
+});
