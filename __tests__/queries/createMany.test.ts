ca5e6a1f8992605a02155248445f3db0fa9e6cae
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-08 08:45:29 +0700

    test(createMany): add comprehensive tests for createMany method with various configurations including timestamps and soft deletes

---
+++ __tests__/queries/createMany.test.ts
@@ -0,0 +1,141 @@
+import DB from "../../src/DB";
+import Model from "../../src/Model";
+import {
+  IMongoloquentSchema,
+  IMongoloquentSoftDelete,
+  IMongoloquentTimestamps,
+} from "../../src/interfaces/ISchema";
+
+beforeEach(async () => {
+  await DB.collection("flights").getCollection().deleteMany({});
+});
+
+afterEach(async () => {
+  await DB.collection("flights").getCollection().deleteMany({});
+});
+
+describe("createMany method", () => {
+  it("without timestamp and soft delete", async () => {
+    interface IFlight extends IMongoloquentSchema {
+      name: string;
+    }
+
+    class Flight extends Model<IFlight> {
+      static $schema: IFlight;
+      static $useTimestamps = false;
+    }
+
+    const flightIds = await Flight.createMany([
+      { name: "Flight 1" },
+      { name: "Flight 2" },
+    ]);
+    expect(flightIds).toEqual(expect.any(Array));
+    expect(flightIds).toHaveLength(2);
+
+    const flights = await Flight.get();
+    expect(flights).toEqual(expect.any(Array));
+    expect(flights).toHaveLength(2);
+
+    const flight = flights[0];
+    expect(flight).toEqual(expect.any(Object));
+    expect(flight).toHaveProperty("_id");
+    expect(flight).toHaveProperty("name", "Flight 1");
+    expect(flight).not.toHaveProperty(Flight.query()["$createdAt"]);
+    expect(flight).not.toHaveProperty(Flight.query()["$updatedAt"]);
+    expect(flight).not.toHaveProperty(Flight.query()["$deletedAt"]);
+  });
+
+  it("with timestamp", async () => {
+    interface IFlight extends IMongoloquentSchema, IMongoloquentTimestamps {
+      name: string;
+    }
+
+    class Flight extends Model<IFlight> {
+      static $schema: IFlight;
+      static $useTimestamps = true;
+    }
+
+    const flightIds = await Flight.createMany([
+      { name: "Flight 1" },
+      { name: "Flight 2" },
+    ]);
+    expect(flightIds).toEqual(expect.any(Array));
+    expect(flightIds).toHaveLength(2);
+
+    const flights = await Flight.get();
+    expect(flights).toEqual(expect.any(Array));
+    expect(flights).toHaveLength(2);
+
+    const flight = flights[0];
+    expect(flight).toEqual(expect.any(Object));
+    expect(flight).toHaveProperty("_id");
+    expect(flight).toHaveProperty("name", "Flight 1");
+    expect(flight).toHaveProperty(Flight.query()["$createdAt"]);
+    expect(flight).toHaveProperty(Flight.query()["$updatedAt"]);
+  });
+
+  it("with soft delete", async () => {
+    interface IFlight extends IMongoloquentSchema, IMongoloquentSoftDelete {
+      name: string;
+    }
+
+    class Flight extends Model<IFlight> {
+      static $schema: IFlight;
+      static $useSoftDelete = true;
+      static $useTimestamps = false;
+    }
+
+    const flightIds = await Flight.createMany([
+      { name: "Flight 1" },
+      { name: "Flight 2" },
+    ]);
+    expect(flightIds).toEqual(expect.any(Array));
+    expect(flightIds).toHaveLength(2);
+
+    const flights = await Flight.get();
+    expect(flights).toEqual(expect.any(Array));
+    expect(flights).toHaveLength(2);
+
+    const flight = flights[0];
+    expect(flight).toEqual(expect.any(Object));
+    expect(flight).toHaveProperty("_id");
+    expect(flight).toHaveProperty("name", "Flight 1");
+    expect(flight).not.toHaveProperty(Flight.query()["$createdAt"]);
+    expect(flight).not.toHaveProperty(Flight.query()["$updatedAt"]);
+    expect(flight).not.toHaveProperty(Flight.query()["$deletedAt"]);
+  });
+
+  it("with soft delete and timestamp", async () => {
+    interface IFlight
+      extends IMongoloquentSchema,
+        IMongoloquentSoftDelete,
+        IMongoloquentTimestamps {
+      name: string;
+    }
+
+    class Flight extends Model<IFlight> {
+      static $schema: IFlight;
+      static $useSoftDelete = true;
+      static $useTimestamps = true;
+    }
+
+    const flightIds = await Flight.createMany([
+      { name: "Flight 1" },
+      { name: "Flight 2" },
+    ]);
+    expect(flightIds).toEqual(expect.any(Array));
+    expect(flightIds).toHaveLength(2);
+
+    const flights = await Flight.get();
+    expect(flights).toEqual(expect.any(Array));
+    expect(flights).toHaveLength(2);
+
+    const flight = flights[0];
+    expect(flight).toEqual(expect.any(Object));
+    expect(flight).toHaveProperty("_id");
+    expect(flight).toHaveProperty("name", "Flight 1");
+    expect(flight).toHaveProperty(Flight.query()["$createdAt"]);
+    expect(flight).toHaveProperty(Flight.query()["$updatedAt"]);
+    expect(flight).not.toHaveProperty(Flight.query()["$deletedAt"]);
+  });
+});
