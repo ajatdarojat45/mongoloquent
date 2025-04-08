1fecbfc91415b42842de1ebcdf29d43b48ef936f
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-09 04:43:06 +0700

    test: add tests for get method in BelongsTo relationship

---
+++ __tests__/relationships/belongsTo/all.test.ts
@@ -0,0 +1,72 @@
+import { ObjectId } from "mongodb";
+
+import DB from "../../../src/DB";
+import Model from "../../../src/Model";
+import {
+  IMongoloquentSchema,
+  IMongoloquentSoftDelete,
+} from "../../../src/interfaces/ISchema";
+
+beforeEach(async () => {
+  await DB.collection("posts").getCollection().deleteMany({});
+  await DB.collection("comments").getCollection().deleteMany({});
+});
+
+afterEach(async () => {
+  await DB.collection("posts").getCollection().deleteMany({});
+  await DB.collection("comments").getCollection().deleteMany({});
+});
+
+describe("all method", () => {
+  it("should return all doc", async () => {
+    interface IPost extends IMongoloquentSchema {
+      title: string;
+      content: string;
+      comments?: Comment[];
+    }
+
+    interface IComment extends IMongoloquentSoftDelete {
+      postId: ObjectId;
+      content: string;
+      active: boolean;
+    }
+
+    class Post extends Model<IPost> {
+      static $collection = "posts";
+      static $schema: Post;
+    }
+
+    class Comment extends Model<IComment> {
+      static $collection = "comments";
+      static $useTimestamps = false;
+      static $schema: Comment;
+
+      post() {
+        return this.belongsTo(Post, "postId", "_id");
+      }
+    }
+
+    const postsIds = await Post.insertMany([
+      { title: "Post 1", content: "Content 1" },
+      { title: "Post 2", content: "Content 2" },
+    ]);
+
+    const commentIds = await Comment.insertMany([
+      { postId: postsIds[0], content: "Comment 1", active: true },
+      { postId: postsIds[0], content: "Comment 2", active: true },
+      { postId: postsIds[1], content: "Comment 3", active: true },
+    ]);
+
+    const comment = await Comment.find(commentIds[0]);
+    const posts = await comment.post().get();
+    expect(posts).toEqual(expect.any(Array));
+    expect(posts).toHaveLength(1);
+    expect(posts[0]).toEqual(
+      expect.objectContaining({
+        _id: postsIds[0],
+        title: "Post 1",
+        content: "Content 1",
+      }),
+    );
+  });
+});
