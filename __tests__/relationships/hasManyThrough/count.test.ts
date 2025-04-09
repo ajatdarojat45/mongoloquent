f149873b9532d60e89f5bc8f8cbefd876e5ebfb2
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-09 17:19:26 +0700

    test: add count method tests for hasManyThrough relationship

---
+++ __tests__/relationships/hasManyThrough/count.test.ts
@@ -0,0 +1,85 @@
+import { ObjectId } from "mongodb";
+
+import DB from "../../../src/DB";
+import Model from "../../../src/Model";
+import { IMongoloquentSchema } from "../../../src/interfaces/ISchema";
+
+beforeEach(async () => {
+  await DB.collection("applications").getCollection().deleteMany({});
+  await DB.collection("environments").getCollection().deleteMany({});
+  await DB.collection("deployments").getCollection().deleteMany({});
+});
+
+afterEach(async () => {
+  await DB.collection("applications").getCollection().deleteMany({});
+  await DB.collection("environments").getCollection().deleteMany({});
+  await DB.collection("deployments").getCollection().deleteMany({});
+});
+
+describe("count method", () => {
+  it("return count document", async () => {
+    interface IApplication extends IMongoloquentSchema {
+      name: string;
+      deployments?: Deployment[];
+    }
+
+    interface IEnveronment extends IMongoloquentSchema {
+      name: string;
+      applicationId: ObjectId;
+    }
+
+    interface Deployment extends IMongoloquentSchema {
+      commit_hash: string;
+      environmentId: ObjectId;
+    }
+
+    class Application extends Model<IApplication> {
+      static $collection: string = "applications";
+      static $schema: IApplication;
+
+      deployments() {
+        return this.hasManyThrough(
+          Deployment,
+          Environment,
+          "applicationId",
+          "environmentId",
+          "_id",
+          "_id",
+        );
+      }
+    }
+
+    class Environment extends Model<IEnveronment> {
+      static $collection: string = "environments";
+      static $schema: IEnveronment;
+    }
+
+    class Deployment extends Model<Deployment> {
+      static $collection: string = "deployments";
+      static $schema: Deployment;
+    }
+
+    const applicationIds = await Application.insertMany([
+      { name: "app1" },
+      { name: "app2" },
+    ]);
+
+    const environmentIds = await Environment.insertMany([
+      { name: "env1", applicationId: applicationIds[0] },
+      { name: "env2", applicationId: applicationIds[0] },
+      { name: "env3", applicationId: applicationIds[1] },
+    ]);
+
+    await Deployment.insertMany([
+      { commit_hash: "123", environmentId: environmentIds[0] },
+      { commit_hash: "456", environmentId: environmentIds[1] },
+      { commit_hash: "789", environmentId: environmentIds[2] },
+    ]);
+
+    const application = await Application.find(applicationIds[0]);
+    const deployments = await application.deployments().count();
+
+    expect(deployments).toEqual(expect.any(Number));
+    expect(deployments).toBe(2);
+  });
+});
