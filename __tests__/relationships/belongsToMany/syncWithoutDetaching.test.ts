fc57db016a7301356280eb8360767628a7914b99
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-10 12:34:26 +0700

    test: add syncWithoutDetaching method test with additional value handling for roles

--- __tests__/relationships/belongsToMany/syncWithoutDetaching.test.ts
+++ __tests__/relationships/belongsToMany/syncWithoutDetaching.test.ts
@@ -65,4 +65,47 @@ describe("syncWithoutDetaching method", () => {
     const rolesAfterDetach = await user.roles().get();
     expect(rolesAfterDetach.length).toBe(3);
   });
+
+  it("syncWithoutDetaching roles to user with value", async () => {
+    const userIds = await User.insertMany([
+      { name: "Udin", email: "udin@mail.com" },
+      { name: "Kosasih", email: "kosasih@mail.com" },
+    ]);
+
+    const roleIds = await Role.insertMany([
+      { name: "Admin", description: "Administrator", likes: 10 },
+      { name: "User", description: "Regular User", likes: 20 },
+      { name: "Guest", description: "Guest User", likes: 5 },
+    ]);
+
+    const user = await User.find(userIds[0]);
+    await user.roles().attach(roleIds[0]);
+    const roles = await user.roles().get();
+    expect(roles.length).toBe(1);
+
+    await user.roles().syncWithoutDetaching([roleIds[1], roleIds[2]], {
+      additional: "value",
+    });
+    const rolesAfterDetach = await user.roles().get();
+    expect(rolesAfterDetach.length).toBe(3);
+
+    const names = [User.name.toLowerCase(), Role.name.toLowerCase()].sort();
+    const pivotCollection = `${names[0]}_${names[1]}`;
+
+    const pivotData = await DB.collection<any>(pivotCollection)
+      .where("userId", userIds[0])
+      .where("roleId", roleIds[1])
+      .first();
+
+    expect(pivotData).toBeDefined();
+    expect(pivotData.additional).toBe("value");
+
+    const pivotData2 = await DB.collection<any>(pivotCollection)
+      .where("userId", userIds[0])
+      .where("roleId", roleIds[0])
+      .first();
+
+    expect(pivotData2).toBeDefined();
+    expect(pivotData2).not.toHaveProperty("additional");
+  });
 });
