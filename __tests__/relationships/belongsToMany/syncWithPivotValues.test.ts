9faab15f9d399ac9b90aa610d230151b99176ac5
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-10 15:02:45 +0700

    test: implement syncWithPivotValues method test for roles to user

--- __tests__/relationships/belongsToMany/syncWithPivotValues.test.ts
+++ __tests__/relationships/belongsToMany/syncWithPivotValues.test.ts
@@ -18,5 +18,64 @@ afterEach(async () => {
 });
 
 describe("syncWithPivotValues method", () => {
-  it("syncWithPivotValues roles to user", async () => {});
+  interface IUser extends IMongoloquentSchema {
+    name: string;
+    email: string;
+    roles?: IRole[];
+  }
+
+  interface IRole extends IMongoloquentSchema {
+    name: string;
+    description: string;
+    likes?: number;
+  }
+
+  class User extends Model<IUser> {
+    static $collection: string = "users";
+    static $schema: IUser;
+
+    roles() {
+      return this.belongsToMany(Role);
+    }
+  }
+
+  class Role extends Model<IRole> {
+    static $collection: string = "roles";
+    static $schema: IRole;
+  }
+
+  const names = [User.name.toLowerCase(), Role.name.toLowerCase()].sort();
+  const pivotCollection = `${names[0]}_${names[1]}`;
+
+  it("syncWithPivotValues roles to user", async () => {
+    const userIds = await User.insertMany([
+      { name: "Udin", email: "udin@mail.com" },
+      { name: "Kosasih", email: "kosasih@mail.com" },
+    ]);
+
+    const roleIds = await Role.insertMany([
+      { name: "Admin", description: "Administrator", likes: 10 },
+      { name: "User", description: "Regular User", likes: 20 },
+      { name: "Guest", description: "Guest User", likes: 5 },
+    ]);
+
+    const user = await User.find(userIds[0]);
+    await user.roles().attach(roleIds[0]);
+    const roles = await user.roles().get();
+    expect(roles.length).toBe(1);
+
+    await user.roles().syncWithPivotValues<{
+      additional: string;
+    }>([roleIds[0], roleIds[1]], { additional: "value" });
+
+    const rolesAfterDetach = await user.roles().get();
+    expect(rolesAfterDetach.length).toBe(2);
+
+    const pivot = await DB.collection<any>(pivotCollection)
+      .where("userId", userIds[0])
+      .first();
+
+    expect(pivot).toBeDefined();
+    expect(pivot.additional).toBe("value");
+  });
 });
