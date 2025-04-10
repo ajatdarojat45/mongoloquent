f7c4e37be9c864e6cf26f04019959631ea14a0c7
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-10 11:10:10 +0700

    test: add comprehensive tests for belongsToMany relationship with various parameter options

---
+++ __tests__/relationships/belongsToMany/get.test.ts
@@ -0,0 +1,348 @@
+import exp from "constants";
+
+import DB from "../../../src/DB";
+import Model from "../../../src/Model";
+import {
+  IMongoloquentSchema,
+  IMongoloquentSoftDelete,
+} from "../../../src/interfaces/ISchema";
+
+beforeEach(async () => {
+  await DB.collection("users").getCollection().deleteMany({});
+  await DB.collection("roles").getCollection().deleteMany({});
+  await DB.collection("role_user").getCollection().deleteMany({});
+});
+
+afterEach(async () => {
+  await DB.collection("users").getCollection().deleteMany({});
+  await DB.collection("roles").getCollection().deleteMany({});
+  await DB.collection("role_user").getCollection().deleteMany({});
+});
+
+describe("get method", () => {
+  describe("without soft delete", () => {
+    interface IUser extends IMongoloquentSchema {
+      name: string;
+      email: string;
+      roles?: IRole[];
+    }
+
+    interface IRole extends IMongoloquentSchema {
+      name: string;
+      description: string;
+    }
+
+    class User extends Model<IUser> {
+      static $collection: string = "users";
+      static $schema: IUser;
+
+      roles() {
+        return this.belongsToMany(Role);
+      }
+    }
+
+    class Role extends Model<IRole> {
+      static $collection: string = "roles";
+      static $schema: IRole;
+    }
+
+    const names = [User.name.toLowerCase(), Role.name.toLowerCase()].sort();
+    const pivotCollection = `${names[0]}_${names[1]}`;
+
+    it("without param", async () => {
+      const userIds = await User.insertMany([
+        { name: "Udin", email: "udin@mail.com" },
+        { name: "Kosasih", email: "kosasih@mail.com" },
+      ]);
+
+      const roleIds = await Role.insertMany([
+        { name: "Admin", description: "Administrator" },
+        { name: "User", description: "Regular User" },
+        { name: "Guest", description: "Guest User" },
+      ]);
+
+      await DB.collection(pivotCollection).insertMany([
+        { userId: userIds[0], roleId: roleIds[0] },
+        { userId: userIds[0], roleId: roleIds[1] },
+        { userId: userIds[1], roleId: roleIds[1] },
+      ]);
+
+      const user = await User.find(userIds[0]);
+      const roles = await user.roles().get();
+      expect(roles).toHaveLength(2);
+      expect(roles[0]).toEqual(expect.any(Object));
+      expect(roles[0]).toHaveProperty("name");
+      expect(roles[0]).toHaveProperty("description");
+
+      const roles2 = await DB.collection(pivotCollection).get();
+      expect(roles2).toEqual(expect.any(Array));
+      expect(roles2).toHaveLength(3);
+    });
+
+    it("with single param", async () => {
+      const userIds = await User.insertMany([
+        { name: "Udin", email: "udin@mail.com" },
+        { name: "Kosasih", email: "kosasih@mail.com" },
+      ]);
+
+      const roleIds = await Role.insertMany([
+        { name: "Admin", description: "Administrator" },
+        { name: "User", description: "Regular User" },
+        { name: "Guest", description: "Guest User" },
+      ]);
+
+      await DB.collection(pivotCollection).insertMany([
+        { userId: userIds[0], roleId: roleIds[0] },
+        { userId: userIds[0], roleId: roleIds[1] },
+        { userId: userIds[1], roleId: roleIds[1] },
+      ]);
+
+      const user = await User.find(userIds[0]);
+      const roles = await user.roles().get("name");
+      expect(roles).toHaveLength(2);
+      expect(roles[0]).toEqual(expect.any(Object));
+      expect(roles[0]).toHaveProperty("name");
+      expect(roles[0]).not.toHaveProperty("description");
+
+      const roles2 = await DB.collection(pivotCollection).get();
+      expect(roles2).toEqual(expect.any(Array));
+      expect(roles2).toHaveLength(3);
+    });
+
+    it("with multiple param", async () => {
+      const userIds = await User.insertMany([
+        { name: "Udin", email: "udin@mail.com" },
+        { name: "Kosasih", email: "kosasih@mail.com" },
+      ]);
+
+      const roleIds = await Role.insertMany([
+        { name: "Admin", description: "Administrator" },
+        { name: "User", description: "Regular User" },
+        { name: "Guest", description: "Guest User" },
+      ]);
+
+      await DB.collection(pivotCollection).insertMany([
+        { userId: userIds[0], roleId: roleIds[0] },
+        { userId: userIds[0], roleId: roleIds[1] },
+        { userId: userIds[1], roleId: roleIds[1] },
+      ]);
+
+      const user = await User.find(userIds[0]);
+      const roles = await user.roles().get("name", "description");
+      expect(roles).toHaveLength(2);
+      expect(roles[0]).toEqual(expect.any(Object));
+      expect(roles[0]).toHaveProperty("name");
+      expect(roles[0]).toHaveProperty("description");
+
+      const roles2 = await DB.collection(pivotCollection).get();
+      expect(roles2).toEqual(expect.any(Array));
+      expect(roles2).toHaveLength(3);
+    });
+
+    it("with array param", async () => {
+      const userIds = await User.insertMany([
+        { name: "Udin", email: "udin@mail.com" },
+        { name: "Kosasih", email: "kosasih@mail.com" },
+      ]);
+
+      const roleIds = await Role.insertMany([
+        { name: "Admin", description: "Administrator" },
+        { name: "User", description: "Regular User" },
+        { name: "Guest", description: "Guest User" },
+      ]);
+
+      await DB.collection(pivotCollection).insertMany([
+        { userId: userIds[0], roleId: roleIds[0] },
+        { userId: userIds[0], roleId: roleIds[1] },
+        { userId: userIds[1], roleId: roleIds[1] },
+      ]);
+
+      const user = await User.find(userIds[0]);
+      const roles = await user.roles().get(["name", "description"]);
+      expect(roles).toHaveLength(2);
+      expect(roles[0]).toEqual(expect.any(Object));
+      expect(roles[0]).toHaveProperty("name");
+      expect(roles[0]).toHaveProperty("description");
+
+      const roles2 = await DB.collection(pivotCollection).get();
+      expect(roles2).toEqual(expect.any(Array));
+      expect(roles2).toHaveLength(3);
+    });
+  });
+
+  describe("without soft delete", () => {
+    interface IUser extends IMongoloquentSchema {
+      name: string;
+      email: string;
+      roles?: IRole[];
+    }
+
+    interface IRole extends IMongoloquentSchema {
+      name: string;
+      description: string;
+    }
+
+    class User extends Model<IUser> {
+      static $collection: string = "users";
+      static $schema: IUser;
+      static $useSoftDelete: boolean = true;
+
+      roles() {
+        return this.belongsToMany(Role);
+      }
+    }
+
+    class Role extends Model<IRole> {
+      static $collection: string = "roles";
+      static $schema: IRole;
+      static $useSoftDelete: boolean = true;
+    }
+
+    const names = [User.name.toLowerCase(), Role.name.toLowerCase()].sort();
+    const pivotCollection = `${names[0]}_${names[1]}`;
+
+    it("without param", async () => {
+      const userIds = await User.insertMany([
+        { name: "Udin", email: "udin@mail.com" },
+        { name: "Kosasih", email: "kosasih@mail.com" },
+      ]);
+
+      const roleIds = await Role.insertMany([
+        { name: "Admin", description: "Administrator" },
+        { name: "User", description: "Regular User" },
+        { name: "Guest", description: "Guest User" },
+      ]);
+
+      await DB.collection(pivotCollection).insertMany([
+        { userId: userIds[0], roleId: roleIds[0] },
+        { userId: userIds[0], roleId: roleIds[1] },
+        { userId: userIds[1], roleId: roleIds[1] },
+      ]);
+
+      await Role.destroy(roleIds[0]);
+
+      const user = await User.find(userIds[0]);
+      const roles = await user.roles().get();
+      expect(roles).toHaveLength(1);
+      expect(roles[0]).toEqual(expect.any(Object));
+      expect(roles[0]).toHaveProperty("name");
+      expect(roles[0]).toHaveProperty("description");
+
+      const roles2 = await Role.get();
+      expect(roles2).toEqual(expect.any(Array));
+      expect(roles2).toHaveLength(2);
+
+      const roles3 = await Role.withTrashed().get();
+      expect(roles3).toEqual(expect.any(Array));
+      expect(roles3).toHaveLength(3);
+    });
+
+    it("with single param", async () => {
+      const userIds = await User.insertMany([
+        { name: "Udin", email: "udin@mail.com" },
+        { name: "Kosasih", email: "kosasih@mail.com" },
+      ]);
+
+      const roleIds = await Role.insertMany([
+        { name: "Admin", description: "Administrator" },
+        { name: "User", description: "Regular User" },
+        { name: "Guest", description: "Guest User" },
+      ]);
+
+      await DB.collection(pivotCollection).insertMany([
+        { userId: userIds[0], roleId: roleIds[0] },
+        { userId: userIds[0], roleId: roleIds[1] },
+        { userId: userIds[1], roleId: roleIds[1] },
+      ]);
+
+      await Role.destroy(roleIds[0]);
+
+      const user = await User.find(userIds[0]);
+      const roles = await user.roles().get("name");
+      expect(roles).toHaveLength(1);
+      expect(roles[0]).toEqual(expect.any(Object));
+      expect(roles[0]).toHaveProperty("name");
+      expect(roles[0]).not.toHaveProperty("description");
+
+      const roles2 = await Role.get();
+      expect(roles2).toEqual(expect.any(Array));
+      expect(roles2).toHaveLength(2);
+
+      const roles3 = await Role.withTrashed().get();
+      expect(roles3).toEqual(expect.any(Array));
+      expect(roles3).toHaveLength(3);
+    });
+
+    it("with multiple param", async () => {
+      const userIds = await User.insertMany([
+        { name: "Udin", email: "udin@mail.com" },
+        { name: "Kosasih", email: "kosasih@mail.com" },
+      ]);
+
+      const roleIds = await Role.insertMany([
+        { name: "Admin", description: "Administrator" },
+        { name: "User", description: "Regular User" },
+        { name: "Guest", description: "Guest User" },
+      ]);
+
+      await DB.collection(pivotCollection).insertMany([
+        { userId: userIds[0], roleId: roleIds[0] },
+        { userId: userIds[0], roleId: roleIds[1] },
+        { userId: userIds[1], roleId: roleIds[1] },
+      ]);
+
+      await Role.destroy(roleIds[0]);
+
+      const user = await User.find(userIds[0]);
+      const roles = await user.roles().get("name", "description");
+      expect(roles).toHaveLength(1);
+      expect(roles[0]).toEqual(expect.any(Object));
+      expect(roles[0]).toHaveProperty("name");
+      expect(roles[0]).toHaveProperty("description");
+
+      const roles2 = await Role.get();
+      expect(roles2).toEqual(expect.any(Array));
+      expect(roles2).toHaveLength(2);
+
+      const roles3 = await Role.withTrashed().get();
+      expect(roles3).toEqual(expect.any(Array));
+      expect(roles3).toHaveLength(3);
+    });
+
+    it("with array param", async () => {
+      const userIds = await User.insertMany([
+        { name: "Udin", email: "udin@mail.com" },
+        { name: "Kosasih", email: "kosasih@mail.com" },
+      ]);
+
+      const roleIds = await Role.insertMany([
+        { name: "Admin", description: "Administrator" },
+        { name: "User", description: "Regular User" },
+        { name: "Guest", description: "Guest User" },
+      ]);
+
+      await DB.collection(pivotCollection).insertMany([
+        { userId: userIds[0], roleId: roleIds[0] },
+        { userId: userIds[0], roleId: roleIds[1] },
+        { userId: userIds[1], roleId: roleIds[1] },
+      ]);
+
+      await Role.destroy(roleIds[0]);
+
+      const user = await User.find(userIds[0]);
+      const roles = await user.roles().get(["name", "description"]);
+      expect(roles).toHaveLength(1);
+      expect(roles[0]).toEqual(expect.any(Object));
+      expect(roles[0]).toHaveProperty("name");
+      expect(roles[0]).toHaveProperty("description");
+
+      const roles2 = await Role.get();
+      expect(roles2).toEqual(expect.any(Array));
+      expect(roles2).toHaveLength(2);
+
+      const roles3 = await Role.withTrashed().get();
+      expect(roles3).toEqual(expect.any(Array));
+      expect(roles3).toHaveLength(3);
+    });
+  });
+});
