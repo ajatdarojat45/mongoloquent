be59d387748aaa27a848d2ccf683865f4c388f6f
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-10 21:26:48 +0700

    test: add tests for min method in morphToMany relationship with and without soft delete

---
+++ __tests__/relationships/morphToMany/min.test.ts
@@ -0,0 +1,122 @@
+import DB from "../../../src/DB";
+import Model from "../../../src/Model";
+import {
+  IMongoloquentSchema,
+  IMongoloquentSoftDelete,
+} from "../../../src/interfaces/ISchema";
+
+beforeEach(async () => {
+  await DB.collection("posts").getCollection().deleteMany({});
+  await DB.collection("tags").getCollection().deleteMany({});
+  await DB.collection("taggables").getCollection().deleteMany({});
+});
+
+afterEach(async () => {
+  await DB.collection("posts").getCollection().deleteMany({});
+  await DB.collection("tags").getCollection().deleteMany({});
+  await DB.collection("taggables").getCollection().deleteMany({});
+});
+
+describe("min method", () => {
+  describe("without soft delete", () => {
+    interface IPost extends IMongoloquentSchema {
+      name: string;
+      tags?: ITag[];
+    }
+
+    interface ITag extends IMongoloquentSchema {
+      name: string;
+      active: boolean;
+      likes: number;
+    }
+
+    class Post extends Model<IPost> {
+      static $collection = "posts";
+      static $schema: IPost;
+
+      tags() {
+        return this.morphToMany(Tag, "taggable");
+      }
+    }
+
+    class Tag extends Model<ITag> {
+      static $collection = "tags";
+      static $schema: ITag;
+      static $useSoftDelete: boolean = true;
+    }
+
+    it("return min of tags", async () => {
+      const postIds = await Post.insertMany([
+        { name: "Post 1" },
+        { name: "Post 2" },
+      ]);
+
+      const tagIds = await Tag.insertMany([
+        { name: "Tag 1", active: true, likes: 5 },
+        { name: "Tag 2", active: false, likes: 10 },
+        { name: "Tag 3", active: true, likes: 2 },
+      ]);
+
+      const post = await Post.find(postIds[0]);
+      await post.tags().attach([tagIds[0], tagIds[1]]);
+
+      const post2 = await Post.find(postIds[1]);
+      await post2.tags().attach([tagIds[0], tagIds[1]]);
+
+      const count = await post.tags().min("likes");
+      expect(count).toBe(5);
+    });
+  });
+
+  describe("with soft delete", () => {
+    interface IPost extends IMongoloquentSchema, IMongoloquentSoftDelete {
+      name: string;
+      tags?: ITag[];
+    }
+
+    interface ITag extends IMongoloquentSchema, IMongoloquentSoftDelete {
+      name: string;
+      active: boolean;
+      likes: number;
+    }
+
+    class Post extends Model<IPost> {
+      static $collection = "posts";
+      static $schema: IPost;
+
+      tags() {
+        return this.morphToMany(Tag, "taggable");
+      }
+    }
+
+    class Tag extends Model<ITag> {
+      static $collection = "tags";
+      static $schema: ITag;
+      static $useSoftDelete: boolean = true;
+    }
+
+    it("return min of tags", async () => {
+      const postIds = await Post.insertMany([
+        { name: "Post 1" },
+        { name: "Post 2" },
+      ]);
+
+      const tagIds = await Tag.insertMany([
+        { name: "Tag 1", active: true, likes: 5 },
+        { name: "Tag 2", active: false, likes: 10 },
+        { name: "Tag 3", active: true, likes: 4 },
+      ]);
+
+      await Tag.destroy(tagIds[0]);
+      const post = await Post.find(postIds[0]);
+      await post.tags().attach([tagIds[0], tagIds[1]]);
+      const post2 = await Post.find(postIds[1]);
+      await post2.tags().attach([tagIds[0], tagIds[1]]);
+      const count = await post.tags().min("likes");
+      expect(count).toBe(10);
+
+      const tags = await Tag.withTrashed().min("likes");
+      expect(tags).toBe(4);
+    });
+  });
+});
