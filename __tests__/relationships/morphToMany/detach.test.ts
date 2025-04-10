977fcab944949f5da1eef0ab1af1e344a20c064f
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-11 04:27:50 +0700

    test: add tests for detach method in MorphToMany relationship

---
+++ __tests__/relationships/morphToMany/detach.test.ts
@@ -0,0 +1,68 @@
+import DB from "../../../src/DB";
+import Model from "../../../src/Model";
+import {
+  IMongoloquentSchema,
+  IMongoloquentSoftDelete,
+} from "../../../src/interfaces/ISchema";
+
+beforeEach(async () => {
+  await DB.collection("posts").getCollection().deleteMany({});
+  await DB.collection("tags").getCollection().deleteMany({});
+  await DB.collection("taggables").getCollection().deleteMany({});
+});
+
+afterEach(async () => {
+  await DB.collection("posts").getCollection().deleteMany({});
+  await DB.collection("tags").getCollection().deleteMany({});
+  await DB.collection("taggables").getCollection().deleteMany({});
+});
+
+describe("detach method", () => {
+  interface IPost extends IMongoloquentSchema {
+    name: string;
+    tags?: ITag[];
+  }
+
+  interface ITag extends IMongoloquentSchema {
+    name: string;
+    active: boolean;
+  }
+
+  class Post extends Model<IPost> {
+    static $collection = "posts";
+    static $schema: IPost;
+
+    tags() {
+      return this.morphToMany(Tag, "taggable");
+    }
+  }
+
+  class Tag extends Model<ITag> {
+    static $collection = "tags";
+    static $schema: ITag;
+  }
+
+  it("detach", async () => {
+    const postIds = await Post.insertMany([
+      { name: "Post 1" },
+      { name: "Post 2" },
+      { name: "Post 3" },
+    ]);
+
+    const tagIds = await Tag.insertMany([
+      { name: "Tag 1", active: true },
+      { name: "Tag 2", active: true },
+      { name: "Tag 3", active: true },
+    ]);
+
+    const post = await Post.find(postIds[0]);
+    await post.tags().attach(tagIds);
+
+    const postTags = await post.tags().get();
+    expect(postTags.length).toBe(3);
+
+    await post.tags().detach(tagIds[0]);
+    const postTagsAfterDetach = await post.tags().get();
+    expect(postTagsAfterDetach.length).toBe(2);
+  });
+});
