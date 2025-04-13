d152a40eb8c91b5568d6d4e738a9dcf5a5d29607
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-13 15:37:23 +0700

    refactor: simplify contains method to use a callback function and improve clarity

--- __tests__/collections/contains.test.ts
+++ __tests__/collections/contains.test.ts
@@ -1,41 +1,29 @@
-import Collection from "../../src/Collection";
+import { collect } from "../../src/index";
 
-describe("Collection.contains", () => {
-  it("should return true when a callback function matches an item", () => {
-    const collection = new Collection(...[{ id: 1 }, { id: 2 }, { id: 3 }]);
-    const result = collection.contains((item) => item.id === 2);
-    expect(result).toBe(true);
+describe("contains", () => {
+  it("with array of numbers", () => {
+    const collection = collect([1, 2, 3, 4, 5]);
+
+    const contains = collection.contains((item) => {
+      return item > 5;
+    });
+
+    expect(contains).toEqual(false);
   });
 
-  it("should return false when a callback function does not match any item", () => {
-    const collection = new Collection(...[{ id: 1 }, { id: 2 }, { id: 3 }]);
-    const result = collection.contains((item) => item.id === 4);
-    expect(result).toBe(false);
-  });
+  it("with array of objects", () => {
+    const collection = collect([
+      { foo: 1 },
+      { foo: 2 },
+      { foo: 3 },
+      { foo: 4 },
+      { foo: 5 },
+    ]);
 
-  it("should return true when a key-value pair matches an item (loose equality)", () => {
-    const collection = new Collection(...[{ id: "1" }, { id: 2 }, { id: 3 }]);
-    const result = collection.contains("id", 1); // Loose equality
-    expect(result).toBe(true);
-  });
+    const contains = collection.contains((item) => {
+      return item.foo > 5;
+    });
 
-  it("should return false when a key-value pair does not match any item", () => {
-    const collection = new Collection(...[{ id: 1 }, { id: 2 }, { id: 3 }]);
-    const result = collection.contains("id", 4);
-    expect(result).toBe(false);
-  });
-
-  it("should return true when a key-value pair matches an item with null or undefined", () => {
-    const collection = new Collection(
-      ...[{ id: null }, { id: undefined }, { id: 3 }]
-    );
-    expect(collection.contains("id", null)).toBe(true);
-    expect(collection.contains("id", undefined)).toBe(true);
-  });
-
-  it("should return false when called on an empty collection", () => {
-    const collection = new Collection<{ id: number }>(...[]);
-    const result = collection.contains("id", 1);
-    expect(result).toBe(false);
+    expect(contains).toEqual(false);
   });
 });
