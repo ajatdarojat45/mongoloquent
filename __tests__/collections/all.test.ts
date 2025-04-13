1c4fe4ee7269dd24c27ae5bcee186ec61d4dc627
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-13 09:51:30 +0700

    refactor: update all method tests to improve clarity and structure

--- __tests__/collections/all.test.ts
+++ __tests__/collections/all.test.ts
@@ -1,41 +1,17 @@
-import Collection from "../../src/Collection";
+import { collect } from "../../src";
 
-describe("Collection - after method", () => {
-  let collection: Collection<{ id: number; name: string }>;
+describe("all method", () => {
+  it("should return all items", () => {
+    const collection = collect([1, 2, 3, 4, 5]);
 
-  beforeEach(() => {
-    collection = new Collection(
-      ...[
-        { id: 1, name: "Alice" },
-        { id: 2, name: "Bob" },
-        { id: 3, name: "Charlie" },
-        { id: 4, name: "David" },
-      ]
-    );
+    const result = collection.all();
+    expect(result).toEqual([1, 2, 3, 4, 5]);
   });
 
-  it("should return the item after the matching key with strict comparison", () => {
-    const result = collection.after("id", 2);
-    expect(result).toEqual({ id: 3, name: "Charlie" });
-  });
+  it("with object", () => {
+    const collection = collect([{ foo: 1 }, { foo: 2 }, { foo: 3 }]);
 
-  it("should return the item after the matching key with loose comparison", () => {
-    const result = collection.after("id", "2", false);
-    expect(result).toEqual({ id: 3, name: "Charlie" });
-  });
-
-  it("should return the item after the matching callback function", () => {
-    const result = collection.after((item) => item.name === "Bob");
-    expect(result).toEqual({ id: 3, name: "Charlie" });
-  });
-
-  it("should return null if no matching item is found", () => {
-    const result = collection.after("id", 99);
-    expect(result).toBeNull();
-  });
-
-  it("should return null if the matching item is the last in the collection", () => {
-    const result = collection.after("id", 4);
-    expect(result).toBeNull();
+    const result = collection.all();
+    expect(result).toEqual([{ foo: 1 }, { foo: 2 }, { foo: 3 }]);
   });
 });
