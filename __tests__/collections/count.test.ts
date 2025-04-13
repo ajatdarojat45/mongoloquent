b9fee5623d86f02f17084b5966e2afc9a0d5e683
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-13 16:50:23 +0700

    refactor: update count tests to use collect and improve clarity

--- __tests__/collections/count.test.ts
+++ __tests__/collections/count.test.ts
@@ -1,38 +1,33 @@
-import Collection from "../../src/Collection";
+import { collect } from "../../src/index";
 
-describe("Collection.count", () => {
-  it("should return the correct count for a non-empty collection", () => {
-    const collection = new Collection(...[1, 2, 3, 4, 5]);
-    const result = collection.count();
-    expect(result).toBe(5);
+describe("count", () => {
+  it("with array of numbers", () => {
+    const collection = collect([1, 2, 3, 4, 5]);
+
+    const count = collection.count();
+
+    expect(count).toEqual(5);
   });
 
-  it("should return 0 for an empty collection", () => {
-    const collection = new Collection(...[]);
-    const result = collection.count();
-    expect(result).toBe(0);
+  it("with array of objects", () => {
+    const collection = collect([
+      { foo: 1 },
+      { foo: 2 },
+      { foo: 3 },
+      { foo: 4 },
+      { foo: 5 },
+    ]);
+
+    const count = collection.count();
+
+    expect(count).toEqual(5);
   });
 
-  it("should return the correct count for a collection with mixed types", () => {
-    const collection = new Collection<
-      number | string | { key: string } | boolean | null
-    >(...[1, "two", { key: "value" }, true, null]);
-    const result = collection.count();
-    expect(result).toBe(5);
-  });
+  it("with empty array", () => {
+    const collection = collect([]);
 
-  it("should return the updated count after adding elements", () => {
-    const collection = new Collection(...[1, 2, 3]);
-    collection.push(4, 5);
-    const result = collection.count();
-    expect(result).toBe(5);
-  });
+    const count = collection.count();
 
-  it("should return the updated count after removing elements", () => {
-    const collection = new Collection(...[1, 2, 3, 4, 5]);
-    collection.pop();
-    collection.pop();
-    const result = collection.count();
-    expect(result).toBe(3);
+    expect(count).toEqual(0);
   });
 });
