5b320778b52ed3b95145b4b15e2baa998058783c
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-14 16:26:40 +0700

    refactor: add test case for implode method with closure for improved coverage

--- __tests__/collections/implode.test.ts
+++ __tests__/collections/implode.test.ts
@@ -12,6 +12,20 @@ describe("implode", () => {
     expect(result).toEqual("Desk, Chair");
   });
 
+  it("with closure", () => {
+    const collection = collect([
+      { accountId: 1, product: "Desk" },
+      { accountId: 2, product: "Chair" },
+    ]);
+
+    const result = collection.implode(
+      (item) => item.product.toUpperCase(),
+      ", ",
+    );
+
+    expect(result).toEqual("DESK, CHAIR");
+  });
+
   it("with array of numbers", () => {
     const collection = collect([1, 2, 3, 4, 5]).implode("-");
 
