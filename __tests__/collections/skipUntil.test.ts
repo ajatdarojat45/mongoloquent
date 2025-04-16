dafea432b5a8fae2845fb6c749795eaaa0ee81c6
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-16 17:58:12 +0700

    refactor: update skipUntil tests to use collect and simplify structure

--- __tests__/collections/skipUntil.test.ts
+++ __tests__/collections/skipUntil.test.ts
@@ -1,33 +1,39 @@
-import Collection from "../../src/Collection";
+import { collect } from "../../src";
 
-describe("Collection.skipUntil", () => {
-  it("should skip items until the callback condition is met", () => {
-    const collection = new Collection(1, 2, 3, 4, 5);
-    const result = collection.skipUntil((item) => item > 3);
-    expect(result).toEqual(new Collection(4, 5));
+describe("skipUntil", () => {
+  it("with array of number", () => {
+    const collection = collect([1, 2, 3, 4]);
+
+    const result = collection.skipUntil((value) => value >= 3);
+
+    expect(result.all()).toEqual([3, 4]);
   });
 
-  it("should return the entire collection if the callback matches the first item", () => {
-    const collection = new Collection(1, 2, 3, 4, 5);
-    const result = collection.skipUntil((item) => item === 1);
-    expect(result).toEqual(new Collection(1, 2, 3, 4, 5));
-  });
+  it("with array of object", () => {
+    const collection = collect([
+      {
+        foo: 1,
+      },
+      {
+        foo: 2,
+      },
+      {
+        foo: 3,
+      },
+      {
+        foo: 4,
+      },
+    ]);
 
-  it("should return only the last item if the callback matches the last item", () => {
-    const collection = new Collection(1, 2, 3, 4, 5);
-    const result = collection.skipUntil((item) => item === 5);
-    expect(result).toEqual(new Collection(5));
-  });
+    const result = collection.skipUntil((value) => value.foo >= 3);
 
-  it("should return an empty collection if the callback matches no items", () => {
-    const collection = new Collection(1, 2, 3, 4, 5);
-    const result = collection.skipUntil((item) => item > 10);
-    expect(result).toEqual(new Collection());
-  });
-
-  it("should return an empty collection if the collection is empty", () => {
-    const collection = new Collection<number>();
-    const result = collection.skipUntil((item) => item > 0);
-    expect(result).toEqual(new Collection());
+    expect(result.all()).toEqual([
+      {
+        foo: 3,
+      },
+      {
+        foo: 4,
+      },
+    ]);
   });
 });
