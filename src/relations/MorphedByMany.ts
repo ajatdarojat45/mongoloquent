12410405a6232a32ae4f6e175797367f2af26a3e
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-24 09:47:46 +0700

    refactor: enhance documentation for MorphedByMany relationship methods and constructor

--- src/relations/MorphedByMany.ts
+++ src/relations/MorphedByMany.ts
@@ -7,6 +7,16 @@ import QueryBuilder from "../QueryBuilder";
 import { IModelPaginate } from "../interfaces/IModel";
 import { IRelationMorphedByMany } from "../interfaces/IRelation";
 
+/**
+ * MorphedByMany relationship class
+ *
+ * Represents a polymorphic many-to-many relationship where the current model can be related to many instances
+ * of other models through a morph table.
+ *
+ * @template T - Type of the parent model
+ * @template M - Type of the related model
+ * @extends {QueryBuilder<M>}
+ */
 export default class MorphedByMany<T, M> extends QueryBuilder<M> {
   model: Model<T>;
   relatedModel: Model<M>;
@@ -15,6 +25,13 @@ export default class MorphedByMany<T, M> extends QueryBuilder<M> {
   morphType: string;
   morphCollectionName: string;
 
+  /**
+   * Creates a new MorphedByMany relationship
+   *
+   * @param {Model<T>} model - The parent model
+   * @param {Model<M>} relatedModel - The related model
+   * @param {string} morph - The base name for the morph relation
+   */
   constructor(model: Model<T>, relatedModel: Model<M>, morph: string) {
     super();
     this.model = model;
@@ -33,16 +50,35 @@ export default class MorphedByMany<T, M> extends QueryBuilder<M> {
     this.$isDeleted = relatedModel["$isDeleted"];
   }
 
+  /**
+   * Retrieves all related records
+   *
+   * @returns {Promise<M[]>} Promise resolving to an array of related model instances
+   */
   public async all(): Promise<M[]> {
     await this.setDefaultCondition();
     return super.all();
   }
 
+  /**
+   * Retrieves related records with specified fields
+   *
+   * @template K - Keys of the related model
+   * @param {...(K | K[])[]} fields - Fields to retrieve
+   * @returns {Promise<Pick<M, K>[]>} Promise resolving to an array of related model instances with selected fields
+   */
   public async get<K extends keyof M>(...fields: (K | K[])[]) {
     await this.setDefaultCondition();
     return super.get(...fields);
   }
 
+  /**
+   * Paginates the related records
+   *
+   * @param {number} page - Page number (starting from 1)
+   * @param {number} limit - Number of records per page
+   * @returns {Promise<IModelPaginate>} Promise resolving to paginated results
+   */
   public async paginate(
     page: number = 1,
     limit: number = 15,
@@ -52,35 +88,81 @@ export default class MorphedByMany<T, M> extends QueryBuilder<M> {
     return super.paginate(page, limit);
   }
 
+  /**
+   * Retrieves the first related record with specified fields
+   *
+   * @template K - Keys of the related model
+   * @param {...(K | K[])[]} fields - Fields to retrieve
+   * @returns {Promise<Pick<M, K> | null>} Promise resolving to the first related record or null
+   */
   public first<K extends keyof M>(...fields: (K | K[])[]) {
     return super.first(...fields);
   }
 
+  /**
+   * Counts the number of related records
+   *
+   * @returns {Promise<number>} Promise resolving to the count of related records
+   */
   public async count(): Promise<number> {
     await this.setDefaultCondition();
     return super.count();
   }
 
+  /**
+   * Calculates the sum of a field in related records
+   *
+   * @template K - Keys of the related model
+   * @param {K} field - The field to sum
+   * @returns {Promise<number>} Promise resolving to the sum
+   */
   public async sum<K extends keyof M>(field: K): Promise<number> {
     await this.setDefaultCondition();
     return super.sum(field);
   }
 
+  /**
+   * Finds the minimum value of a field in related records
+   *
+   * @template K - Keys of the related model
+   * @param {K} field - The field to find minimum value for
+   * @returns {Promise<number>} Promise resolving to the minimum value
+   */
   public async min<K extends keyof M>(field: K): Promise<number> {
     await this.setDefaultCondition();
     return super.min(field);
   }
 
+  /**
+   * Finds the maximum value of a field in related records
+   *
+   * @template K - Keys of the related model
+   * @param {K} field - The field to find maximum value for
+   * @returns {Promise<number>} Promise resolving to the maximum value
+   */
   public async max<K extends keyof M>(field: K): Promise<number> {
     await this.setDefaultCondition();
     return super.max(field);
   }
 
+  /**
+   * Calculates the average value of a field in related records
+   *
+   * @template K - Keys of the related model
+   * @param {K} field - The field to calculate average for
+   * @returns {Promise<number>} Promise resolving to the average value
+   */
   public async avg<K extends keyof M>(field: K): Promise<number> {
     await this.setDefaultCondition();
     return super.avg(field);
   }
 
+  /**
+   * Sets default condition for relation queries based on the parent model
+   *
+   * @private
+   * @returns {Promise<void>}
+   */
   private async setDefaultCondition() {
     const mbmColl = this["getCollection"](this.morphCollectionName);
 
