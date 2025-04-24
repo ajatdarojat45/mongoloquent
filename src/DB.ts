b26b4c633ccf4dd39c0b08885d2103e9faaa8581
Ajat Darojat <ajatdarojat45@gmail.com>
2025-04-24 10:14:16 +0700

    refactor: enhance documentation for DB class connection and database methods

--- src/DB.ts
+++ src/DB.ts
@@ -21,10 +21,69 @@ export default class DB<T> extends QueryBuilder<T> {
   }
 
   /**
+   * Sets the connection URI for database operations
+   *
+   * @template T - The type of documents stored in the collection
+   * @param {string} uri - The MongoDB connection URI
+   * @returns {DB<T>} A new DB instance with the specified connection
+   * @static
+   */
+  public static connection<T>(
+    this: new () => DB<T>,
+    connection: string,
+  ): DB<T> {
+    const q = new this();
+    q["$connection"] = connection;
+
+    return q;
+  }
+
+  /**
+   * Sets the connection URI for database operations on the current instance
+   *
+   * @param {string} uri - The MongoDB connection URI
+   * @returns {this} The current DB instance for method chaining
+   */
+  public connection(connection: string) {
+    this["$connection"] = connection;
+
+    return this;
+  }
+
+  /**
+   * Sets the database name for subsequent operations
+   *
+   * @template T - The type of documents stored in the collection
+   * @param {string} database - The name of the database to use
+   * @returns {DB<T>} A new DB instance with the specified database
+   * @static
+   */
+  public static database<T>(this: new () => DB<T>, database: string): DB<T> {
+    const q = new this();
+    q["$databaseName"] = database;
+
+    return q;
+  }
+
+  /**
+   * Sets the database name for operations on the current instance
+   *
+   * @param {string} database - The name of the database to use
+   * @returns {this} The current DB instance for method chaining
+   */
+  public database(database: string) {
+    this["$databaseName"] = database;
+
+    return this;
+  }
+
+  /**
    * Creates a new DB instance for the specified collection
+   *
    * @template T - The type of documents stored in the collection
    * @param {string} collection - The name of the collection to operate on
    * @returns {DB<T>} A new DB instance configured for the specified collection
+   * @static
    */
   public static collection<T>(
     this: new () => DB<T>,
@@ -37,7 +96,20 @@ export default class DB<T> extends QueryBuilder<T> {
   }
 
   /**
+   * Sets the collection name for operations on the current instance
+   *
+   * @param {string} collection - The name of the collection to operate on
+   * @returns {this} The current DB instance for method chaining
+   */
+  public collection(collection: string) {
+    this["$collection"] = collection;
+
+    return this;
+  }
+
+  /**
    * Performs a MongoDB $lookup aggregation to join documents from another collection
+   *
    * @param {IDBLookup} document - The lookup configuration containing from, localField, foreignField, and as properties
    * @returns {this} The current DB instance for method chaining
    */
@@ -51,6 +123,7 @@ export default class DB<T> extends QueryBuilder<T> {
 
   /**
    * Adds raw MongoDB aggregation pipeline stages to the query
+   *
    * @param {Document | Document[]} documents - One or more MongoDB aggregation pipeline stages
    * @returns {this} The current DB instance for method chaining
    */
