c02668d7073171eb6f777e140b610fbd154fab7c
Ajat Darojat <ajatdarojat45@gmail.com>
2025-07-31 21:07:49 +0700

    feat: implement HasOne relationship class for MongoDB query handling

---
+++ src/relationships/has-one.relationship.ts
@@ -0,0 +1,138 @@
+import { Document } from "mongodb";
+import { Collection, Model, QueryBuilder } from "../core";
+import { IQueryBuilderPaginated, IRelationshipHasOne } from "../types";
+import { LookupBuilder } from "./index";
+
+export class HasOne<T, M> extends QueryBuilder<M> {
+	model: Model<T>;
+	relatedModel: Model<M>;
+	foreignKey: keyof M;
+	localKey: keyof T;
+
+	constructor(
+		model: Model<T>,
+		relatedModel: Model<M>,
+		foreignKey: keyof M,
+		localKey: keyof T,
+	) {
+		super();
+		this.model = model;
+		this.relatedModel = relatedModel;
+		this.foreignKey = foreignKey;
+		this.localKey = localKey;
+
+		this.$connection = relatedModel["$connection"];
+		this.$collection = relatedModel["$collection"];
+		this.$useSoftDelete = relatedModel["$useSoftDelete"];
+		this.$databaseName = relatedModel["$databaseName"];
+		this.$useSoftDelete = relatedModel["$useSoftDelete"];
+		this.$useTimestamps = relatedModel["$useTimestamps"];
+		this.setIsDeleted(relatedModel["$isDeleted"]);
+	}
+
+	public all(): Promise<Collection<M>> {
+		this.where(this.foreignKey, this.model["$original"][this.localKey]);
+		return super.all();
+	}
+
+	public get<K extends keyof M>(
+		...fields: (K | (string & {}) | (K | (string & {}))[])[]
+	): Promise<Collection<M>> {
+		this.where(this.foreignKey, this.model["$original"][this.localKey]);
+		return super.get(...fields);
+	}
+
+	public paginate(
+		page: number = 1,
+		limit: number = 15,
+	): Promise<IQueryBuilderPaginated> {
+		this.where(this.foreignKey, this.model["$original"][this.localKey]);
+		return super.paginate(page, limit);
+	}
+
+	public first<K extends keyof M>(
+		...fields: (K | (string & {}) | (K | (string & {}))[])[]
+	) {
+		this.where(this.foreignKey, this.model["$original"][this.localKey]);
+		return super.first(...fields);
+	}
+
+	public count(): Promise<number> {
+		this.where(this.foreignKey, this.model["$original"][this.localKey]);
+		return super.count();
+	}
+
+	public sum<K extends keyof M>(field: K | (string & {})): Promise<number> {
+		this.where(this.foreignKey, this.model["$original"][this.localKey]);
+		return super.sum(field);
+	}
+
+	public min<K extends keyof M>(field: K | (string & {})): Promise<number> {
+		this.where(this.foreignKey, this.model["$original"][this.localKey]);
+		return super.min(field);
+	}
+
+	public max<K extends keyof M>(field: K | (string & {})): Promise<number> {
+		this.where(this.foreignKey, this.model["$original"][this.localKey]);
+		return super.max(field);
+	}
+
+	public avg<K extends keyof M>(field: K | (string & {})): Promise<number> {
+		this.where(this.foreignKey, this.model["$original"][this.localKey]);
+		return super.avg(field);
+	}
+
+	static generate(hasOne: IRelationshipHasOne): Document[] {
+		const lookup = this.lookup(hasOne);
+
+		if (hasOne.options?.select) {
+			const select = LookupBuilder.select(hasOne.options.select, hasOne.alias);
+			lookup.push(...select);
+		}
+
+		if (hasOne.options?.exclude) {
+			const exclude = LookupBuilder.exclude(
+				hasOne.options.exclude,
+				hasOne.alias,
+			);
+			lookup.push(...exclude);
+		}
+
+		return lookup;
+	}
+
+	static lookup(hasOne: IRelationshipHasOne): Document[] {
+		const lookup: Document[] = [];
+		const pipeline: Document[] = [];
+
+		// Add soft delete condition to the pipeline if enabled
+		if (hasOne.relatedModel["$useSoftDelete"]) {
+			pipeline.push({
+				$match: {
+					$expr: {
+						$and: [{ $eq: [`$${hasOne.relatedModel["$isDeleted"]}`, false] }],
+					},
+				},
+			});
+		}
+
+		const $lookup = {
+			from: hasOne.relatedModel["$collection"],
+			localField: hasOne.localKey,
+			foreignField: hasOne.foreignKey,
+			as: hasOne.alias,
+			pipeline: pipeline,
+		};
+
+		lookup.push({ $lookup });
+
+		lookup.push({
+			$unwind: {
+				path: `$${hasOne.alias}`,
+				preserveNullAndEmptyArrays: true,
+			},
+		});
+
+		return lookup;
+	}
+}
