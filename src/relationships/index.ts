4d8560fc921141317c7c5cebcc06fa68895f4be2
Ajat Darojat <ajatdarojat45@gmail.com>
2025-08-01 06:51:52 +0700

    feat: reintroduce HasMany and add HasManyThrough, BelongsTo, and BelongsToMany exports in relationships index

--- src/relationships/index.ts
+++ src/relationships/index.ts
@@ -1,3 +1,6 @@
 export * from "./lookup-builder.relationship";
-export * from "./has-many.relationship";
 export * from "./has-one.relationship";
+export * from "./has-many.relationship";
+export * from "./has-many-through.relationship";
+export * from "./belongs-to.relationship";
+export * from "./belongs-to-many.relationship";
