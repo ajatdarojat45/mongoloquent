81de5ffbb0a7145abd9b5e5122a25bcc16bf1314
Ajat Darojat <ajatdarojat45@gmail.com>
2025-11-18 22:33:56 +0700

    feat: enhance relationship interfaces with generic model support

--- src/types/relationships.type.ts
+++ src/types/relationships.type.ts
@@ -1,4 +1,15 @@
 import { Model } from "../core";
+import {
+	BelongsTo,
+	BelongsToMany,
+	HasMany,
+	HasManyThrough,
+	HasOne,
+	MorphedByMany,
+	MorphMany,
+	MorphTo,
+	MorphToMany,
+} from "../relationships";
 
 export enum IRelationshipTypes {
 	hasOne = "hasOne",
@@ -12,122 +23,148 @@ export enum IRelationshipTypes {
 	morphedByMany = "morphedByMany",
 }
 
-export interface IRelationshipOptions {
-	select?: string | string[];
-	exclude?: string | string[];
-	makeVisible?: string | string[];
-	sort?: [string, "asc" | "desc"];
+export interface IRelationshipOptions<T = any> {
+	select?: keyof T | (keyof T)[];
+	exclude?: keyof T | (keyof T)[];
+	makeVisible?: keyof T | (keyof T)[];
+	sort?: [keyof T | (string & {}), "asc" | "desc"];
 	skip?: number;
 	limit?: number;
 }
 
-export interface IRelationshipHasOne {
+export interface IRelationshipHasOne<T> {
 	type: IRelationshipTypes.hasOne;
 	model: Model;
-	relatedModel: Model;
+	relatedModel: Model<T>;
 	foreignKey: string;
 	localKey: string;
 	alias: string;
-	options: IRelationshipOptions;
+	options: IRelationshipOptions<T>;
 	nested?: string[];
 }
 
-export interface IRelationshipBelongsTo {
+export interface IRelationshipBelongsTo<T> {
 	type: IRelationshipTypes.belongsTo;
 	model: Model;
-	relatedModel: Model;
+	relatedModel: Model<T>;
 	foreignKey: string;
 	ownerKey: string;
 	alias: string;
-	options: IRelationshipOptions;
+	options: IRelationshipOptions<T>;
 	nested?: string[];
 }
 
-export interface IRelationshipHasMany {
+export interface IRelationshipHasMany<T> {
 	type: IRelationshipTypes.hasMany;
 	model: Model;
-	relatedModel: Model;
+	relatedModel: Model<T>;
 	foreignKey: string;
 	localKey: string;
 	alias: string;
-	options: IRelationshipOptions;
+	options: IRelationshipOptions<T>;
 	nested?: string[];
 }
 
-export interface IRelationshipHasManyThrough {
+export interface IRelationshipHasManyThrough<T> {
 	type: IRelationshipTypes.hasManyThrough;
 	model: Model;
-	relatedModel: Model;
+	relatedModel: Model<T>;
 	throughModel: Model;
 	foreignKey: string;
 	foreignKeyThrough: string;
 	localKey: string;
 	localKeyThrough: string;
 	alias: string;
-	options: IRelationshipOptions;
+	options: IRelationshipOptions<T>;
 	nested?: string[];
 }
 
-export interface IRelationshipBelongsToMany {
+export interface IRelationshipBelongsToMany<T> {
 	type: IRelationshipTypes.belongsToMany;
 	model: Model;
-	relatedModel: Model;
+	relatedModel: Model<T>;
 	pivotModel: Model;
 	foreignPivotKey: string;
 	relatedPivotKey: string;
 	parentKey: string;
 	relatedKey: string;
 	alias: string;
-	options: IRelationshipOptions;
+	options: IRelationshipOptions<T>;
 	nested?: string[];
 }
 
-export interface IRelationshipMorphTo {
+export interface IRelationshipMorphTo<T> {
 	type: IRelationshipTypes.morphTo;
 	model: Model;
-	relatedModel: Model;
+	relatedModel: Model<T>;
 	morph: string;
 	morphId: string;
 	morphType: string;
 	alias: string;
-	options: IRelationshipOptions;
+	options: IRelationshipOptions<T>;
 	nested?: string[];
 }
 
-export interface IRelationshipMorphMany {
+export interface IRelationshipMorphMany<T> {
 	type: IRelationshipTypes.morphMany;
 	model: Model;
-	relatedModel: Model;
+	relatedModel: Model<T>;
 	morph: string;
 	morphId: string;
 	morphType: string;
 	alias: string;
-	options: IRelationshipOptions;
+	options: IRelationshipOptions<T>;
 	nested?: string[];
 }
 
-export interface IRelationshipMorphToMany {
+export interface IRelationshipMorphToMany<T> {
 	type: IRelationshipTypes.morphToMany;
 	model: Model;
-	relatedModel: Model;
+	relatedModel: Model<T>;
 	morph: string;
 	morphId: string;
 	morphType: string;
 	morphCollectionName: string;
 	alias: string;
-	options: IRelationshipOptions;
+	options: IRelationshipOptions<T>;
 	nested?: string[];
 }
 
-export interface IRelationshipMorphedByMany {
+export interface IRelationshipMorphedByMany<T> {
 	type: IRelationshipTypes.morphedByMany;
 	model: Model;
-	relatedModel: Model;
+	relatedModel: Model<T>;
 	morph: string;
 	morphId: string;
 	morphType: string;
 	morphCollectionName: string;
 	alias: string;
-	options: IRelationshipOptions;
+	options: IRelationshipOptions<T>;
 	nested?: string[];
 }
+
+export type ExtractRelationshipType<T, K extends keyof T> = T[K] extends (
+	...args: any[]
+) => HasOne<any, infer R>
+	? R
+	: T[K] extends (...args: any[]) => HasMany<any, infer R>
+		? R
+		: T[K] extends (...args: any[]) => BelongsTo<any, infer R>
+			? R
+			: T[K] extends (...args: any[]) => BelongsToMany<any, infer R, any>
+				? R
+				: T[K] extends (...args: any[]) => HasManyThrough<any, infer R, any>
+					? R
+					: T[K] extends (...args: any[]) => MorphMany<any, infer R>
+						? R
+						: T[K] extends (...args: any[]) => MorphTo<any, infer R>
+							? R
+							: T[K] extends (...args: any[]) => MorphToMany<any, infer R>
+								? R
+								: T[K] extends (...args: any[]) => MorphedByMany<any, infer R>
+									? R
+									: any;
+
+export type RelationshipKeys<T> = {
+	[K in keyof T]: T[K] extends (...args: any[]) => any ? K : never;
+}[keyof T];
